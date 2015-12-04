f7b1e0cc6b4937cea98184b17d71d57203ef392a
Luis Fuentes <angel_5fuentes@outlook.com>
2015-12-04 06:49:58 -0800

    Update project4.java

--- project4.java
+++ project4.java
@@ -2,18 +2,23 @@
 ////luis fuentes
 ////cst 112
 //// Arrays of objects.
-
-
+Rat rt;
+Bird f;
+Button one, two, three, four;
 int nb=16;
 Ball[] b=  new Ball[nb];         // Array of Ball objects.
 
-int nh=10;
+/*int nh=10;
 Button[] h=  new Button[nh];    // Array of Button objects.
-
+*/
 float left, right, top, bottom, middle;
 
 WTable t;
 Cloud[] cloudList;
+
+int frame;
+int score;
+
 
 //// SETUP:  create arrays, initialize, ec.
 void setup() {
@@ -26,6 +31,9 @@ void setup() {
    t.middle= t.left + (t.right-t.left) / 2;
    t.horizon= (height/4)-20;
    t.wall= false ;
+   
+   f=new Bird();
+   rt= new Rat();
   // Create cue and 15 balls.
   b[0]=  new Ball( 0, 250, 250, 250 );
   b[0].x=  200;
@@ -38,16 +46,29 @@ void setup() {
   for (int i=1; i<nb; i++) {
     b[i]=  new Ball( 0, 250, 250, 250 );
   }
-  h[0]=  new Button( 20, 25 );
+  /*for (int i=1; i<nh; i++) {h[0]=  new Button( 20, 25 );}
+  /*
   for (int i=1; i<nh; i++) {
     h[i]=  new Button( 120,25 );
-  }
+  }*/
     cloudList= new Cloud[7];
   float cloudX=50;
   for( int i=0; i<cloudList.length; i++) {
      cloudList[i]=  new Cloud( cloudX,random(t.horizon-100,t.horizon-25));
      cloudX += 100;
   }
+   ///buttons
+   one= new Button(50,5);
+   one.words= "Reset";
+  
+   two= new Button(140,5);
+   two.words= "Wall";
+   
+   three= new Button(230,5);
+   three.words="Bird";
+    
+   four= new Button(320,5);
+   four.words="Rat";
 }
 
 //// NEXT:  Draw ball, after moving and colliding.
@@ -59,7 +80,11 @@ void draw() {
   action();
   show();
   messages();
- 
+  bird();
+  rat();
+  buttons();
+  frame +=1;
+  showScore();
   
 }
 
@@ -67,17 +92,30 @@ void draw() {
 void scene() {
 }
 
+void rat(){
+  rt.moveRat();
+  rt.showRat();
+  rt.scoreFix();
+}
 //// ACTION:  move, collide, etc.
 void action() {
   for( int i=0; i<nb-1; i++) {
-    for( int j=i+1; j<nb; j++) {    
+    for( int j=i+1; j<nb; j++) { 
+    
       collide( b[i], b[j] );
+     
     }
   }
   //// Move all balls.
   for( int i=0; i<nb; i++) {
    b[i].move();
   }
+}
+
+void bird(){
+  f.show();
+  f.bombDrop();
+  f.moveBird();
 }
 //// Elastic collisions -- swap dx,dy
 void collide( Ball p, Ball q ) {
@@ -87,24 +125,71 @@ void collide( Ball p, Ball q ) {
     tmp=p.dy;  p.dy=q.dy;  q.dy=tmp;
     noStroke();
     fill(250,250,250,250);
-    ellipse(b[0].x,b[0].y,40,40);
+    ellipse(p.x,p.y,40,40);
     
   }//++++
 }
 
+void ring( Ball p, Ball q){
+   if ( p.hit( q.x,q.y ) ){
+     float tmp;
+    tmp=p.dx;  p.dx=q.dx;  q.dx=tmp;     
+    tmp=p.dy;  p.dy=q.dy;  q.dy=tmp; 
+    noStroke();
+    fill(250,250,250,250);
+    ellipse(p.x,p.y,40,40);
+  }
+}
+    
+void ratCollision( Ball p, Rat q ) {
+  if ( p.hit( q.x,q.y ) ) {
+    p.dx=0;
+    p.dy=0;
+    if (rt.scoreBuffer == false){          //convoluted fix for the problem with the rat collision scoring.
+      score -= 10;                         // b/c the rat doesn't instantly go in the opposite direction of the ball
+      rt.scoreBufferCounter = 0;           // there is a 'collision' for many frames in a row, causing the score to plumet.
+      rt.scoreBuffer = true;               // w/ the fix, the -10 score can only happen every 10 frames, usually enough time 
+    }                                      // for the rat to move away from the ball
+  }
+}
+
+//DISPLAY BUTTONS
+  void buttons(){
+    one.buttonDisplay();
+    two.buttonDisplay();
+    three.buttonDisplay();
+    four.buttonDisplay();
+}
+void showScore(){
+  text("Score", 50, height-50);
+  text(score , 90, height -50);
+}
 //// Display 
 void show() {
   for( int i=0; i<nb; i++) {
     b[i].show();
   }
-  for( int i=0; i<nh; i++) {
-    h[i].buttonDisplay();}
+ /* for( int i=0; i<nh; i++) {
+    h[i].buttonDisplay();}*/
 }
 
+void reset() {
+
+   b[0].randomize();
+   t.wall = true;
+   
+}
 //// ????
 void messages() {
 }
 
+void mousePressed() {
+   rt.clickRat();
+   one.buttonReset();
+   two.buttonWall();
+   three.buttonBird();
+   four.buttonRat();
+ }
 
  ///showing the clouds
 void drawClouds(){
@@ -147,11 +232,7 @@ class Ball {
   }
   
   //// METHODS ////
-  /*void move() {
-    //+++++
-    x += dx;
-    y += dy;
-  }*/
+ 
     void move() {
     if (t.wall) {                        //if the wall is there
       if (x>t.right-4 || x<t.middle+20) 
@@ -177,6 +258,35 @@ class Ball {
     fill(r,g,b);
     ellipse( x, y, 15, 15 );
   }
+    void reset() {                       //puts the the balls on right side random position
+    x=  random( (width/2)+60, t.right );    
+    y=  random( t.top, t.bottom );
+    dx=  random( -4, 4 );
+    dy=  random( -3, 2 );
+  }
+    void resetCue() {            //puts the cue ball on the left side centered not moving
+    x= width/4;
+    y= (t.bottom+t.top)/2;
+    dx= 0; 
+    dy= 0;
+  }
+   void clickBall(){
+    if (dist( mouseX, mouseY, x,y) < 15){
+       x=  random( (width/2)+60, t.right );    
+       y=  random( t.top, t.bottom );
+       dx=  random( -6,2 );
+       dy=  random( -3,4 );
+       score -=5;
+    }
+  }
+    void clickCue(){
+    if (dist( mouseX, mouseY, x,y) < 15){
+       x= width/4;
+       y= (t.bottom+t.top)/2;
+       dx= 0; dy= 0;
+       score -=5;
+    }
+    }
    boolean hit( float x, float y ) {                        
     if (  dist( x,y, this.x,this.y ) < 15 ) return true;   
     else return false;
@@ -225,17 +335,45 @@ class Button {
     fill(100,120,130);
     noStroke();
     rectMode( CORNER );
-    rect(x, y, 80, 40);/*
+    rect(x, y, 80, 40);
    fill(255);
     textSize(16);
     text( words, x+25, y+25);
-    textSize(13);*/
+    textSize(13);
   }
   
-  
+   void buttonReset(){
+    if (mouseX >x && mouseX<x+80 && mouseY>y && mouseY<y+40){
+    reset();
+  }
+ }
+ //no wall
+ void buttonWall(){
+  if (mouseX >x && mouseX<x+80 && mouseY>y && mouseY<y+40){
+    t.wall = false;
+  }
+ }
+ 
+ void buttonBird(){
+   if (counter == true) {
+   buffer +=1;
+       } 
+   if (mouseX >x && mouseX<x+80 && mouseY>y && mouseY<y+40){
+     f.fly = true;
+     counter = true;
+     if (buffer > 1){
+     f.drop = true;
+     }
+   }
+ }
+  void buttonRat(){
+   if (mouseX >x && mouseX<x+80 && mouseY>y && mouseY<y+40){
+     rt.crawl = true;
+   }
+ }
 }
 
-/*class Rat {   float x,y,DX,DY;
+class Rat {   float x,y,DX,DY;
   boolean crawl;
   boolean scoreBuffer;
   int scoreBufferCounter;
@@ -295,7 +433,7 @@ class Button {
       }
     }
   }  
-}*/
+}
 
 class Cloud{
   
@@ -335,7 +473,7 @@ class Cloud{
     }
   }
 }
-/*class Bird {
+class Bird {
   //// PROPERTIES:  position, speed, color, etc. ////   (What a Bird "has".)
   float x=0,y=50,by,bDY;
   float w=60;
@@ -384,6 +522,7 @@ class Cloud{
   }
    void bombDrop(){
     if (drop==true){
+       
         noStroke();
         fill(105);
         rect(x,by-50,25,50);
@@ -403,4 +542,4 @@ class Cloud{
     else return true;
   }
   
-}*/
+}
